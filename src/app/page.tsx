4bd26dd38972a95fece6a7e15dce7cfd864585ad
buarki <92484888+buarki@users.noreply.github.com>
2024-01-03 02:29:31 -0300

    chore: mark origin and destination (#1) (#2)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Grid from "@path-craft/components/grid.component";
+import { GRID_COLUMNS, GRID_ROWS } from "@path-craft/configs";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Grid rows={19} cols={35}/>
+      <Grid rows={GRID_ROWS} cols={GRID_COLUMNS}/>
     </main>
   )
 }
--- src/components/grid.component.tsx
+++ src/components/grid.component.tsx
@@ -1,17 +1,26 @@
 "use client"
 
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import Cell from './cell.component';
+import { GRID_COLUMNS, GRID_ROWS } from '@path-craft/configs';
+
+function randomNumberBetween(lowerBound: number, upperBound: number) {
+  return Math.floor(Math.random() * (upperBound - lowerBound + 1)) + lowerBound;
+}
 
 interface GridProps {
   rows: number;
   cols: number;
 }
 
+const invalidPoint = { x: -1, y: -1 };
+
 const Grid: React.FC<GridProps> = ({ rows, cols }) => {
   const [grid, setGrid] = useState<Array<Array<boolean>>>(() =>
     Array.from({ length: rows }, () => Array(cols).fill(false))
   );
+  const [origin, setOrigin] = useState<{x: number, y: number}>(invalidPoint);
+  const [destination, setDestination] = useState<{x: number, y: number}>(invalidPoint);
 
   const [isMousePressed, setMousePressed] = useState<boolean>(false);
 
@@ -29,6 +38,32 @@ const Grid: React.FC<GridProps> = ({ rows, cols }) => {
     }
   };
 
+  const reloadGrid = useCallback(() => {
+    const newGrid = Array.from({ length: rows }, () => Array(cols).fill(false));
+
+    let newOriginX = randomNumberBetween(0, GRID_ROWS - 1);
+    let newOriginY = randomNumberBetween(0, GRID_COLUMNS - 1);
+
+    let newDestinationX = randomNumberBetween(0, GRID_ROWS - 1);
+    let newDestinationY = randomNumberBetween(0, GRID_COLUMNS - 1);
+
+    while (newOriginX === newDestinationX && newOriginY === newDestinationY) {
+      newOriginX = randomNumberBetween(0, GRID_ROWS - 1);
+      newOriginY = randomNumberBetween(0, GRID_COLUMNS - 1);
+      newDestinationX = randomNumberBetween(0, GRID_ROWS - 1);
+      newDestinationY = randomNumberBetween(0, GRID_COLUMNS - 1);
+    }
+
+    setOrigin({ x: newOriginX, y: newOriginY });
+    setDestination({ x: newDestinationX, y: newDestinationY });
+
+    setGrid(newGrid);
+  }, [cols, rows]);
+
+  useEffect(() => {
+    reloadGrid();
+  }, [reloadGrid]);
+
   return (
     <div
       className="grid grid-cols-1 gap-1"
@@ -40,6 +75,8 @@ const Grid: React.FC<GridProps> = ({ rows, cols }) => {
           {row.map((isWall, colIndex) => (
             <div key={colIndex} className='m-1'>
               <Cell
+                isOrigin={rowIndex === origin.x && colIndex === origin.y}
+                isDestination={rowIndex == destination.x && colIndex === destination.y}
                 isWall={isWall}
                 onClick={() => handleCellClick(rowIndex, colIndex)}
                 onMouseOver={() => handleCellHover(rowIndex, colIndex)}
@@ -48,6 +85,13 @@ const Grid: React.FC<GridProps> = ({ rows, cols }) => {
           ))}
         </div>
       ))}
+      <div className='flex justify-center gap-12'>
+        <button
+          onClick={reloadGrid}
+          className='bg-blue-500 hover:bg-blue-300 p-2 text-white font-bold'>Reload</button>
+        <button
+          className='bg-green-500 hover:bg-green-300 p-2 text-white font-bold'>Find Path</button>
+      </div>
     </div>
   );
 };
