aad49f41b3c008355502cf7bd1be2f4773919025
buarki <92484888+buarki@users.noreply.github.com>
2024-01-03 17:20:09 -0300

    feat: highligh found path (#7) (#15)

--- src/components/cell.component.tsx
+++ src/components/cell.component.tsx
@@ -1,24 +1,40 @@
 "use client";
 
-import React from 'react';
-
 interface CellProps {
   isWall: boolean;
   isOrigin: boolean;
   isDestination: boolean;
   beingUsedDuringSearch: boolean;
+  isOptimalPath: boolean;
   onClick: () => void;
   onMouseOver: () => void;
 }
 
-const Cell: React.FC<CellProps> = ({ isWall, isOrigin, isDestination, beingUsedDuringSearch, onClick, onMouseOver, }) => {
+const Cell: React.FC<CellProps> = ({
+  isWall,
+  isOrigin,
+  isDestination,
+  beingUsedDuringSearch,
+  isOptimalPath,
+  onClick,
+  onMouseOver,
+}) => {
   const cellColor =
-    beingUsedDuringSearch ? 'bg-gray-800' :
-    isOrigin ? 'bg-yellow-200' :
-    isDestination ? 'bg-green-300' : isWall ? 'bg-red-800' : 'bg-gray-200';
+    beingUsedDuringSearch
+      ? 'bg-gray-800'
+      : isOrigin
+      ? 'bg-yellow-200'
+      : isDestination
+      ? 'bg-green-300'
+      : isWall
+      ? 'bg-red-500'
+      : 'bg-gray-200';
+
+  const optimalPathStyle = isOptimalPath ? 'bg-purple-500' : '';
+
   return (
     <div
-      className={`w-8 h-8 ${cellColor}`}
+      className={`w-8 h-8 ${cellColor} ${optimalPathStyle}`}
       onClick={onClick}
       onMouseOver={onMouseOver}
     />
