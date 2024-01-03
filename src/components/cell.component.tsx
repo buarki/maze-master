f694dddf93214f9f16c2d4b359166e772d0e652b
buarki <oreasekadd@gmail.com>
2024-01-03 02:28:54 -0300

    chore: mark origin and destination (#1)

--- src/components/cell.component.tsx
+++ src/components/cell.component.tsx
@@ -4,14 +4,17 @@ import React from 'react';
 
 interface CellProps {
   isWall: boolean;
+  isOrigin: boolean;
+  isDestination: boolean;
   onClick: () => void;
   onMouseOver: () => void;
 }
 
-const Cell: React.FC<CellProps> = ({ isWall, onClick, onMouseOver }) => {
+const Cell: React.FC<CellProps> = ({ isWall, isOrigin, isDestination, onClick, onMouseOver, }) => {
+  const cellColor = isOrigin ? 'bg-red-300' : isDestination ? 'bg-green-300' : isWall ? 'bg-gray-800' : 'bg-gray-200';
   return (
     <div
-      className={`w-8 h-8 ${isWall ? 'bg-gray-800' : 'bg-gray-200'}`}
+      className={`w-8 h-8 ${cellColor}`}
       onClick={onClick}
       onMouseOver={onMouseOver}
     />
---
+++ src/configs/index.ts
@@ -0,0 +1,2 @@
+export const GRID_ROWS = 19;
+export const GRID_COLUMNS = 35;
