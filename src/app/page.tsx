b97b0d43663c47d01314a63b8ae3a90cba5bd64a
buarki <92484888+buarki@users.noreply.github.com>
2024-01-03 07:18:31 -0300

    chore: adjust import alias (#10) (#11)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Grid from "@path-craft/components/grid.component";
-import { GRID_COLUMNS, GRID_ROWS } from "@path-craft/configs";
+import Grid from "@maze-master/components/grid.component";
+import { GRID_COLUMNS, GRID_ROWS } from "@maze-master/configs";
 
 export default function Home() {
   return (
