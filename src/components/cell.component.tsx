ec7c044e29ad7666439c0e765b1e4196fdd4a3fd
buarki <92484888+buarki@users.noreply.github.com>
2024-01-03 03:38:19 -0300

    chore: animate path finding seach (#3) (#4)

--- src/components/cell.component.tsx
+++ src/components/cell.component.tsx
@@ -6,12 +6,16 @@ interface CellProps {
   isWall: boolean;
   isOrigin: boolean;
   isDestination: boolean;
+  beingUsedDuringSearch: boolean;
   onClick: () => void;
   onMouseOver: () => void;
 }
 
-const Cell: React.FC<CellProps> = ({ isWall, isOrigin, isDestination, onClick, onMouseOver, }) => {
-  const cellColor = isOrigin ? 'bg-red-300' : isDestination ? 'bg-green-300' : isWall ? 'bg-gray-800' : 'bg-gray-200';
+const Cell: React.FC<CellProps> = ({ isWall, isOrigin, isDestination, beingUsedDuringSearch, onClick, onMouseOver, }) => {
+  const cellColor =
+    beingUsedDuringSearch ? 'bg-yellow-300' :
+    isOrigin ? 'bg-red-300' :
+    isDestination ? 'bg-green-300' : isWall ? 'bg-gray-800' : 'bg-gray-200';
   return (
     <div
       className={`w-8 h-8 ${cellColor}`}
--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,2 +1,3 @@
 export const GRID_ROWS = 19;
 export const GRID_COLUMNS = 35;
+export const SEARCH_SPEED = 30;
